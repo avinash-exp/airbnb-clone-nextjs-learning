d46aef864308ae5d67617f1c4091c66aaeb58492
avinash-exp <avinash2jan@gmail.com>
2024-05-21 20:59:15 +0200

    added UI layout for Search and Menu Items

--- airnb/app/components/navbar/Navbar.tsx
+++ airnb/app/components/navbar/Navbar.tsx
@@ -1,5 +1,9 @@
+"use client";
+
 import Container from "../Container";
 import Logo from "./Logo";
+import Search from "./Search"
+import UserMenu from "./UserMenu";
 
 const Navbar = () => {
     return (
@@ -15,13 +19,15 @@ const Navbar = () => {
                         className="
                             flex
                             flex-row
-                            item-center
+                            items-center
                             justify-between
                             gap-3
                             md:gap-0
                         "
                     >
                         <Logo/>
+                        <Search/>
+                        <UserMenu/>
                     </div>
                 </Container>
             </div>
